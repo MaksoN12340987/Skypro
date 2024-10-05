75612d3d224b334c3334d9f1bcd1c8ee58320201
Maxim Andreev <andrianov_maksim@outlook.com>
2024-10-05 11:12:51 +0300

    home data tupe

--- homeWork/todoLessons/test.py
+++ homeWork/todoLessons/test.py
@@ -1,1 +1,16 @@
-print("Py")
+number = int(input())
+
+def EndNumberOne (number_in):
+   number_in = number_in % 10
+   if number_in == 1:
+      return True
+   else:
+      return False
+
+print(EndNumberOne(number))
+
+def FileTupe (file_name):
+   if ".gif" in file_name:
+      return True
+   else:
+      return False
