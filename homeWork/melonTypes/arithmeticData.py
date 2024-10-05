e6bff0861702962810cbdd5994558fb7e1ae3aa6
Maksim Andreev <andrianov_maksim@outlook.com>
2024-10-05 11:26:08 +0300

    Dopoln

---
+++ homeWork/melonTypes/arithmeticData.py
@@ -0,0 +1,31 @@
+print("Cappuchino --", 80)
+print("Cookis --", 40)
+print("Bun --", 60)
+print("Sum --", 80+40+60)
+print("Tax fee --",((80+40+60)/100)*13)
+
+print("5 \n4 \n3 \n2 \n1 ")
+
+print("Поехали!")
+print("Целочисленное деление //")
+a = 31 // 6 
+print(a)
+print("Остаток от деления %")
+a = 31 % 6 
+print(a)
+print("Возведение в степень **")
+a = 2 ** 6 
+print(a)
+print("Инкремент +=")
+a = 10
+a += 2
+print(a)
+print("Декремент -=")
+a = 10
+a -= 2
+print(a)
+
+prise = input("Какова цена продукта? ")
+myStr = "Iscrime prise " + str(prise)
+print(myStr)
+
