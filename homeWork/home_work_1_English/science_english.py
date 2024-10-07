cfc73bbddace8e8fe7f1f5d35c47af6d24d24020
Maxim Andreev <andrianov_maksim@outlook.com>
2024-10-07 20:39:50 +0300

    Home work 1 csience English not working Status_count

--- homeWork/home_work_1_English/science_english.py
+++ homeWork/home_work_1_English/science_english.py
@@ -1,48 +1,55 @@
 # Создание переменных
-question_count = 0
+coll_question = 3
 question_reward = 10
 count_true_questions = 0
 
 # Блок вопросов
-def Question_exists_is(name_in, count_in, true_count):
+def Question_exists_is(name_in):
+    true_count = 0
     word_in = input(f"Какое слово пропущено?\nMy name ___ {name_in}. \n")
     if word_in == "is":
-        print("Ответ верный!\nВы получаете 10 баллов!")
+        print("Ответ верный!\nВы получаете 10 баллов! \n")
         true_count += 1
     else:
-        print(f"Неправильно.\nПравильный ответ: is")
-    count_in += 1
+        print(f"Неправильно.\nПравильный ответ: is \n")
     return true_count
         
 def Question_exists_am():
+    true_count = 0
     word_in = input(f"Какое слово пропущено?\nI ___ a coder. \n")
     if word_in == "am":
         print("Ответ верный!\nВы получаете 10 баллов! \n")
+        true_count += 1
     else:
-        print(f"Неправильно.\nПравильный ответ: am")
+        print(f"Неправильно.\nПравильный ответ: am \n")
+    return true_count
         
 def Question_exists_in():
+    true_count = 0
     word_in = input(f"Какое слово пропущено?\nI live ___ Moscow. \n")
     if word_in == "in":
-        print("Ответ верный!\nВы получаете 10 баллов!")
+        print("Ответ верный!\nВы получаете 10 баллов! \n")
+        true_count += 1
     else:
-        print(f"Неправильно.\nПравильный ответ: in")
+        print(f"Неправильно.\nПравильный ответ: in \n")
+    return true_count
         
-def Status_count(count_question_in, count_true_questions_in, reward_in):
-    print(f"Вы ответили на {count_true_questions_in} вопросов из {count_question_in} верно.")
-    print(f"Вы заработали {count_question_in * reward_in} баллов.")
-    
-    
-# end def
+def Status_count(coll_question_in, count_true_questions_in, reward_in):
+    print(f"Вы ответили на {count_true_questions_in} вопросов из {coll_question_in} верно.")
+    print(f"Вы заработали {count_true_questions_in * reward_in} баллов.")
+    fracton_true_question = (coll_question_in / count_true_questions_in) * 100
+    print(f"Это {fracton_true_question} процентов.")
+
     
 # Выполняем основной блок приветствия пользователю
 name_user = input("Привет! Предлагаю проверить свои знания английского!\nНапиши, как тебя зовут? \n")
-print(f"Привет, {name_user}, начинаем тренировку!")
+print(f"Привет, {name_user}, начинаем тренировку! \n")
 
 # Выполняем блок вопосов
-Question_exists_is(name_user)
-Question_exists_am()
-Question_exists_in()
+count_true_questions += Question_exists_is(name_user)
+count_true_questions += Question_exists_am()
+count_true_questions += Question_exists_in()
 
 print(f"Вот и всё, {name_user}!")
-Status_count(question_count, count_true_questions, question_reward)
+Status_count(coll_question, count_true_questions, question_reward)
+
