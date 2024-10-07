2a40ecbcb444c9bd4485ee3f5e8af9c17186faeb
Maxim Andreev <andrianov_maksim@outlook.com>
2024-10-07 17:12:55 +0300

    Home work 1 csience English add DEF 1, 2, 3

--- homeWork/home_work_1_English/science_english.py
+++ homeWork/home_work_1_English/science_english.py
@@ -3,15 +3,31 @@ question_count = 0
 
 # Блок вопросов
 def Question_exists_is(name_in):
-    word_in = input(f"Какое слово пропущено?\nMy name ___ {name_in}")
+    word_in = input(f"Какое слово пропущено?\nMy name ___ {name_in}. \n")
     if word_in == "is":
         print("Ответ верный!\nВы получаете 10 баллов!")
     else:
         print(f"Неправильно.\nПравильный ответ: is")
+        
+def Question_exists_am():
+    word_in = input(f"Какое слово пропущено?\nI ___ a coder. \n")
+    if word_in == "am":
+        print("Ответ верный!\nВы получаете 10 баллов! \n")
+    else:
+        print(f"Неправильно.\nПравильный ответ: am")
+        
+def Question_exists_in():
+    word_in = input(f"Какое слово пропущено?\nI live ___ Moscow. \n")
+    if word_in == "in":
+        print("Ответ верный!\nВы получаете 10 баллов!")
+    else:
+        print(f"Неправильно.\nПравильный ответ: in")
     
-# Вывод приветствия пользователю
-name_user = ans = input("Привет! Предлагаю проверить свои знания английского!\nНапиши, как тебя зовут? ")
+# Выполняем основной блок приветствия пользователю
+name_user = ans = input("Привет! Предлагаю проверить свои знания английского!\nНапиши, как тебя зовут? \n")
 print(f"Привет, {name_user}, начинаем тренировку!")
 
-# Выполняем программу
+# Выполняем блок вопосов
 Question_exists_is(name_user)
+Question_exists_am()
+Question_exists_in()
