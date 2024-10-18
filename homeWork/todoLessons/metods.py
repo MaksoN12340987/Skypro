ce10aedb7746b0dd34ac3de810457a3346289927
Maxim Andreev <andrianov_maksim@outlook.com>
2024-10-18 10:20:24 +0300

    add metod.py

---
+++ homeWork/todoLessons/metods.py
@@ -0,0 +1,11 @@
+colors = ["красный", "оранжевый", "красный", "синий", "красный", "черный"]
+
+index = 0
+while index < len(colors) - 1:
+    colors.remove("красный")
+    index += 1
+
+# Не удаляйте код ниже и не редактируйте его, он нужен для проверки
+
+for color in colors:
+  print(color)
