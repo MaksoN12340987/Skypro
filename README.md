acaa54326f99c6b24fa9e306ce6790eab6a0ef89
Maxim Andreev <andrianov_maksim@outlook.com>
2024-10-24 10:08:46 +0300

    add to readme

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ str() — строка
 float() — дробное число
 
 round() — округляет число
+
+# добавление элемента в список при этом после него элементы i++
+list.insert(index, element) 
+
+# позволяет вернуть индекс
+index = my_list.index(3) 
+list.index(element_to_find, start_index, end_index) '''не включая end_index'''
+print(index)
+
+# колличество элементов
+list.count(element)
--- homeWork/todoLessons/metods.py
+++ homeWork/todoLessons/metods.py
@@ -1,11 +1,14 @@
-colors = ["красный", "оранжевый", "красный", "синий", "красный", "черный"]
+# colors = ["красный", "оранжевый", "красный", "синий", "красный", "черный"]
 
-index = 0
-while index < len(colors) - 1:
-    colors.remove("красный")
-    index += 1
+# index = 0
+# while index < len(colors) - 1:
+#     colors.remove("красный")
+#     index += 1
 
-# Не удаляйте код ниже и не редактируйте его, он нужен для проверки
+# # Не удаляйте код ниже и не редактируйте его, он нужен для проверки
 
-for color in colors:
-  print(color)
+# for color in colors:
+  # print(color)
+
+
+print('''text in "dead"''')
