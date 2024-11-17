6b65f31e7f8412cc011bd0dabc39861d49952a64
Maksim Andreev <andrianov_maksim@outlook.com>
2024-11-17 16:11:20 +0300

    final work, section 1

--- homeWork/home_work_4_final_tasks/final_tasks.py
+++ homeWork/home_work_4_final_tasks/final_tasks.py
@@ -1,8 +1,6 @@
 # Напишите функцию count_morse_characters(), которая принимает строку, состоящую из символов азбуки Морзе, и 
 # возвращает количество символов (точек и/или тире) в ней
 def count_morse_characters(input_string):
-    input_string.replace(" ", "")
-
     point_counter = 0
     dash_counter = 0
     for i in range(len(input_string)):
@@ -11,7 +9,8 @@ def count_morse_characters(input_string):
         else:
             dash_counter += 1
     
-    return (f"В строке {len(input_string)} символов, точек: {point_counter}, тире: {dash_counter} ")
+    return (point_counter + dash_counter)
 
-string_morse = count_morse_characters(input())
-print(string_morse)
+
+string_morse = input().replace(" ", "")
+print(count_morse_characters(string_morse))
