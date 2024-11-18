9ce48630c17c109859df30702d97311ce6c9dc9f
Maksim Andreev <andrianov_maksim@outlook.com>
2024-11-19 01:58:41 +0300

    final work, section 1, task5

--- homeWork/home_work_4_final_tasks/final_tasks.py
+++ homeWork/home_work_4_final_tasks/final_tasks.py
@@ -10,12 +10,7 @@
         
 #         else:
 #             continue
-    
 #     return (point_counter + dash_counter)
-
-
-# string_morse = input().replace(" ", "")
-# print(count_morse_characters(string_morse))
 
 
 # Создайте функцию morse_encode(), которая принимает словарь с азбукой Морзе и слово для кодирования 
@@ -34,8 +29,6 @@
 #     for i in range(len(word)):
 #         finaly += dictionary[word[i]] + " "
 #     return finaly
-
-# print(morse_encode(morse, words_to_decode[0]))
 
 
 # Вам предоставлен словарь соответствия символов строки коду азбуки Морзе.
@@ -57,20 +50,12 @@
 # Напишите функцию calculate_total_cost(), которая принимает список продуктов 
 # и возвращает общую стоимость всех товаров (цена * количество).
 # Если у продукта не указаны цена и/или количество, это не должно приводить к ошибке.
-products = [
-    {"name": "Apple", "category": "fruit", "price": 120, "quantity": 10},
-    {"name": "Banana", "category": "fruit", "price": 90, "quantity": 15},
-    {"name": "Avocado", "category": "fruit", "price": 200, "quantity": 5},
-    {"name": "Tomato", "category": "veggie", "price": 100, "quantity": 20},
-    {"name": "Broccoli", "category": "veggie", "quantity": 8},
-    {"name": "Carrot", "category": "veggie", "price": 100, "quantity": 25}
-]
+
 
 # def calculate_total_cost(ist_products):
 #     total_prise = 0
 #     prise = 0
 #     quantity = 0
-
 #     for i in range(len(ist_products)):
 #         keys_list = list(ist_products[i].keys())
 #         for index in range(len(keys_list)):
@@ -80,7 +65,6 @@ products = [
 #             elif keys_list[index] == "quantity":
 #                 quantity = ist_products[i][keys_list[index]]
 #                 print(quantity)
-                
 #         if prise == 0:
 #             prise = 1
 #         elif quantity == 0:
@@ -89,25 +73,34 @@ products = [
 #             continue
 #         if prise != 0 and quantity != 0:
 #             total_prise += prise * quantity
-
 #     return total_prise
-
-# print(calculate_total_cost(products))
 
 
 # Напишите функцию filter_products_by_price(), которая принимает на вход список продуктов и верхний порог цены и 
 # возвращает список продуктов, цена которых не превышает заданную максимальную цену. Если у продукта не указана цена, 
 # это не должно приводить к ошибке, при получении значения по ключу, 
 # если ключа в словаре нет - цена должна равняться 0 (используйте метод get())
-# def filter_products_by_price(ist_products, maximum_price):
+products = [
+    {"name": "Apple", "category": "fruit", "price": 120, "quantity": 10},
+    {"name": "Banana", "category": "fruit", "price": 90, "quantity": 15},
+    {"name": "Avocado", "category": "fruit", "price": 200, "quantity": 5},
+    {"name": "Tomato", "category": "veggie", "price": 100, "quantity": 20},
+    {"name": "Broccoli", "category": "veggie", "price": 300, "quantity": 8},
+    {"name": "Carrot", "category": "veggie", "price": 100},
+    {"name": "Cookie", "category": "sweets", "price": 200, "quantity": 12, "brand": "ABC"},
+    {"name": "Donut", "category": "sweets", "price": 300, "quantity": 7, "brand": "XYZ"},
+    {"name": "Cake", "category": "sweets", "price": 400, "quantity": 3, "brand": "DEF", "discount": 10},
+    {"name": "Orange", "category": "fruit", "price": 150, "quantity": 18},
+    {"name": "Lettuce", "category": "veggie", "price": 80, "quantity": 30, "organic": True},
+    {"name": "Chocolate", "category": "sweets", "price": 250, "quantity": 10, "brand": "GHI", "flavor": "Dark"}
+]
+
+# def filter_products_by_price(list_products, maximum_price):
 #     finaly_list = []
 #     for i in range(len(ist_products)):
 #         if ist_products[i].get("price", 0) <= maximum_price:
 #             finaly_list.append(ist_products[i])
 #     return finaly_list
-                
-
-# print(filter_products_by_price(products, 170))
 
 
 # Напишите функцию find_product_by_name(), которая принимает список продуктов и имя для поиска и 
@@ -134,35 +127,44 @@ products = [
 # За один вызов функция может обновить только один продукт. Если продукт не найден в списке, 
 # функция возвращает строку «Продукт с таким именем не найден в списке».
 # Если у продукта отсутствует имя, это не должно привести к ошибке
-products = [
-    {"name": "Apple", "category": "fruit", "price": 120, "quantity": 10},
-    {"name": "Banana", "category": "fruit", "price": 90, "quantity": 15},
-    {"name": "Avocado", "category": "fruit", "price": 200, "quantity": 5},
-    {"name": "Tomato", "category": "veggie", "price": 100, "quantity": 20},
-    {"name": "Broccoli", "category": "veggie", "price": 300, "quantity": 8},
-    {"name": "Carrot", "category": "veggie", "price": 100, "quantity": 25},
-    {"name": "Cookie", "category": "sweets", "price": 200, "quantity": 12, "brand": "ABC"},
-    {"name": "Donut", "category": "sweets", "price": 300, "quantity": 7, "brand": "XYZ"},
-    {"name": "Cake", "category": "sweets", "price": 400, "quantity": 3, "brand": "DEF", "discount": 10},
-    {"name": "Orange", "category": "fruit", "price": 150, "quantity": 18},
-    {"name": "Lettuce", "category": "veggie", "price": 80, "quantity": 30, "organic": True},
-    {"name": "Chocolate", "category": "sweets", "price": 250, "quantity": 10, "brand": "GHI", "flavor": "Dark"}
-]
 
-def update_product_info(ist_product, name_product, dict_data):
-    dict_find_humber = None
-    final = {}
+# replace_dict = {"name": "REPLASE", "price": 250, "quantity": 10, "brand": "GHI", "flavor": "Dark"}
+
+# def update_product_info(list_product, name_product, dict_data):
+#     final = False
+#     for i in range(len(ist_product)):
+#         if ist_product[i].get("name", 0) == name_product:
+#             final = True
+#             keys_list = list(dict_data.keys())
+#             final = ist_product[i]
+#             for count in range(len(dict_data)):
+#                 ist_product[i][keys_list[count]] = dict_data[keys_list[count]]
+#     if final:
+#         return ist_product
+#     else:
+#         return "Продукт с таким именем не найден в списке"
     
-    for i in range(len(ist_product)):
-        if ist_product[i].get("name", 0) == name_product:
-            dict_find_humber = i
-    
-    if len(dict_data) <= len(ist_product[dict_find_humber]):
-        key_product = 0
-        key_find = 0
-        for couht in range(len(dict_data)):
-            final[].add(element)
-    else:
-        couht = len(ist_product[dict_find_humber])
-    
-    
+  
+#   Напишите функцию sort_products_by_quantity(). Функция должна принимать на вход список продуктов и 
+# направление сортировки (атрибут должен иметь имя ascending) со значением по умолчанию False (булевое значение) и 
+# сортировать продукты по количеству в порядке возрастания или убывания.
+# Если в функцию не передан аргумент направления сортировки, сортировка должна проходить в порядке возрастания количества товаров 
+# (от меньшего к большему). Если у продукта не указано количество, это не должно привести к ошибке, при получении значения по ключу, 
+# если ключа в словаре нет - количество должно равняться 0 (используйте метод get())
+
+# def sort_products_by_quantity(list_product, ascending=False):
+#     print(len(ist_product))
+#     if ist_product[0]:
+#         for i in range(len(ist_product)):
+#             if not ist_product[i].get("quantity", False):
+#                 item = ist_product.pop(ist_product[i])
+#         ist_product.sort(key=lambda ist_product: ist_product["quantity"], reverse=ascending)
+#     return ist_product
+
+
+# Напишите функцию average_price_per_category(), которая принимает на вход список продуктов и возвращает новый словарь, 
+# где ключи — категории, а значения — средняя цена продуктов в каждой категории. 
+# Округлите результат вычисления до 1 знака после запятой. 
+# Если у продукта отсутствует категория, это не должно привести к ошибке
+def average_price_per_category(list_product):
+    finaly_dict = {}
